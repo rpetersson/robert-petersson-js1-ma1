060aab2e71c535b6617e5f8b0073eeacee87f55a
PETERSSON Robert <robert.petersson@soprasteria.com>
2021-01-19 22:03:56 +0100

    testing# Please enter the commit message for your changes. Lines starting

---
+++ script.js
@@ -0,0 +1,20 @@
+//question 1
+const cat = {
+    complain : function meow() {
+        console.log("Meow!");
+}
+}
+
+//question 2
+
+var heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+//question 3
+
+heading.style.fontSize = "2em";
+
+//question 4
+
+heading.classList.add("subheading");
